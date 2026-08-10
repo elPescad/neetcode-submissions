476a1d29196c6861c236728c8594d161f8d319ab
David Salazar <das255@njit.edu>
2026-08-10 11:41:01 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> mp;
+
+        for(int i = 0; i < strs.size(); i++) {
+            string original = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            mp[strs[i]].push_back(original);
+        }
+
+        for(auto vec: mp) {
+            res.push_back(vec.second);
+        }
+
+        return res;
+    }
+};
