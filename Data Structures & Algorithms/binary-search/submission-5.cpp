d97e60c315266acbc5654fd9ea6d26a4040bdb5d
David Salazar <das255@njit.edu>
2026-08-14 23:57:10 -0400

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = left + (right-left)/2;
+        while(left <= right) {
+            mid = left + (right-left)/2;
+
+            if(nums[mid] < target) {
+                left = mid+1;
+            } else if(nums[mid] > target) {
+                right = mid-1;
+            } else {
+                break;
+            }
+        }
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+        return -1;
+    }
+};
