247abeed0656625d14d637f26efab46b84e4ab23
David Salazar <das255@njit.edu>
2026-09-08 15:24:56 -0400

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size(), node = 0;
+        vector<int> dist(n, INT_MAX);
+        vector<int> visit(n, false);
+        int edges = 0, res = 0;
+
+        while(edges < n-1) {
+            visit[node] = true;
+            int nextNode = -1;
+            for(int i = 0; i < n; i++) {
+                if(visit[i]) continue;
+                int curDistance = abs(points[i][0] - points[node][0])
+                                    + abs(points[i][1] - points[node][1]);
+                dist[i] = min(dist[i], curDistance);
+                if(nextNode == -1 || dist[i] < dist[nextNode]) {
+                    nextNode = i;
+                }
+            }
+            res += dist[nextNode];
+            node = nextNode;
+            edges++;
+        }
+        return res;
+    }
+};
