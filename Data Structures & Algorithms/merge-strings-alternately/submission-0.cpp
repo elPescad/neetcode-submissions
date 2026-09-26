12b46cfcc8f2c4f9c663b6fd205cfad944f07afb
David Salazar <das255@njit.edu>
2026-09-25 23:40:28 -0400

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int w1 = 0, w2 = 0;
+        string res = "";
+        while(w1 < word1.size() && w2 < word2.size()) {
+            res += word1[w1++];
+            res += word2[w2++];
+        }
+
+        while(w1 < word1.size()) {
+            res += word1[w1++];
+        }
+
+        while(w2 < word2.size()) {
+            res += word2[w2++];
+        }
+        return res;
+    }
+};
